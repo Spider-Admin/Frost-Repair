c43d5ee51a40c31edc0082fdefcc93ad54be8f0f
Spider-Admin <Spider-Admin@proton.me>
2022-08-16 12:05:30 +0200

    Update year in header

--- src/main/java/org/frost/repair/Main.java
+++ src/main/java/org/frost/repair/Main.java
@@ -1,5 +1,5 @@
 /*
-  Copyright 2020 - 2021 Spider-Admin@Z+d9Knmjd3hQeeZU6BOWPpAAxxs
+  Copyright 2021 - 2022 Spider-Admin@Z+d9Knmjd3hQeeZU6BOWPpAAxxs
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
